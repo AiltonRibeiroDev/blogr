e98f1ac6d42ed2afa9f55974ed0941a999041a8a
AiltonRibeiroDev <ribeiroailtonalves@gmail.com>
2023-08-26 19:56:40 -0300

    Projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 let burguer = document.getElementById("burguer");
-let btnLink = document.getElementById('nav-btn-links');
+let btnLink = document.getElementById("nav-btn-links");
+let acessDiv = document.getElementById("acess");
 
-function menuOpenClose(){
-   
-    if(btnLink.style.display == 'flex'){
-        btnLink.style.display = 'none'
-    }else{
-        btnLink.style.display = 'flex'
-    }
+function menuOpenClose() {
+  if (btnLink.style.display == "flex") {
+    btnLink.style.display = "none";
+    btnLink.classList.remove("config-mobile");
+  } else {
+    btnLink.style.display = "flex";
+    btnLink.classList.add("config-mobile");
+  }
 }
-
